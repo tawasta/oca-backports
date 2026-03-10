d9fee8b8e2b53cf0beecef972539f0dba9aea7f1
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-03-10 14:10:01 +0200

    [MIG] 19.0 website_legal_page

---
+++ website_legal_page/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2015 Antonio Espinosa <antonioea@antiun.com>
+# Copyright 2017 Tecnativa - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website Legal Page",
+    "category": "Website",
+    "version": "19.0.1.0.0",
+    "depends": ["website"],
+    "data": [
+        "views/website_legal_main_page.xml",
+    ],
+    "author": """Tecnativa, LasLabs, Nicolas JEUDY,
+    Odoo Community Association (OCA), Futural""",
+    "website": "https://github.com/tawasta/oca-backports",
+    "license": "AGPL-3",
+}
